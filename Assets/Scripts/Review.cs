1c6150b6cd73672323741f4e558e81f89df3e9aa
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-11 13:11:57 -0500

    Reviewed variables

--- Assets/Scripts/Review.cs
+++ Assets/Scripts/Review.cs
@@ -7,12 +7,27 @@ public class Review : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Integers are truncated (rounded down) whereas floats store decimal numbers
+        int integer = 1 / 2;
+        float fraction = 1.0f / 2.0f;
+        Debug.Log(integer);
+        Debug.Log(fraction);
+
+        // Doubles have twice the precision of floats, so they can store much larger numbers accurately!
+        Debug.Log(0.9999999999999);
+        Debug.Log(0.9999999999999f);
+
+        // Different data-types have different sizes (sizeof tells you how many bytes each data-type stores).
+        Debug.Log("Size of single integer: " + sizeof(int));
+        Debug.Log("Size of single decimal: " + sizeof(float));
+        Debug.Log("Size of single double-precision decimal: " + sizeof(double));
+        Debug.Log("Size of single character: " + sizeof(char));
+        Debug.Log("Size of single byte: " + sizeof(byte));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World!");
     }
 }
