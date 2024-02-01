4763e2c4c3d311ad1212e1c55c0e2358073d3ca7
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-01 12:48:47 -0500

    Inheritance & polymorphism review

--- Assets/Scripts/Polymorphism.cs
+++ Assets/Scripts/Polymorphism.cs
@@ -48,6 +48,59 @@ public class Fox : Animal
     }
 }
 
+public abstract class Shape
+{
+    public abstract float Area();
+    public abstract string Name();
+}
+
+public class Triangle : Shape
+{
+    public override float Area()
+    {
+        return (width * height) / 2.0f;
+    }
+
+    public override string Name()
+    {
+        return "Jimmy";
+    }
+
+    public float width;
+    public float height;
+}
+
+public class Rectangle : Shape
+{
+    public override float Area()
+    {
+        return width * height;
+    }
+
+    public override string Name()
+    {
+        return "Harold";
+    }
+
+    public float width;
+    public float height;
+}
+
+public class Circle : Shape
+{
+    public override float Area()
+    {
+        return 2.0f * Mathf.PI * radius;
+    }
+
+    public override string Name()
+    {
+        return "Jennifer Coolidge";
+    }
+
+    public float radius;
+}
+
 // Homework: Using polymorphism, modify the final exam solution so
 // that each weapon is its own class with an overwritten Fire() method.
 // Marks -- 1 mark per weapon (3% total),
@@ -85,9 +138,26 @@ public class Polymorphism : MonoBehaviour
         }
     }
 
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Triangle triangle = new Triangle();
+        Rectangle rectangle = new Rectangle();
+        Circle circle = new Circle();
+
+        triangle.width = rectangle.width = 50.0f;
+        triangle.height = rectangle.height = 100.0f;
+        circle.radius = 25.0f;
+        Debug.Log("Triangle area: " + triangle.Area());
+        Debug.Log("Rectangle area: " + rectangle.Area());
+        Debug.Log("Circle area: " + circle.Area());
+
+        Shape[] shapes = { triangle, rectangle, circle };
+        for (int i = 0; i < shapes.Length; i++)
+            Debug.Log("Area of " + shapes[i].Name() + ": " + shapes[i].Area());
+
         // Objects interpreted as Animal can only have behaviour common to all animals.
         // (Only the Dog can growl but since dog is an Animal, dog.Growl() is an error).
         //dog.Growl();
