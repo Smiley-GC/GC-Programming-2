62f5bdea44bd5abfeb5235d0c83d5a70146999b0
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-02 13:49:07 -0500

    Added file-spliting example

---
+++ Assets/Scripts/Weapons2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Example of how to separate behaviour across multiple files
+public static class Weapons2
+{
+    public static GameObject CreateRifle()
+    {
+        return null;
+    }
+
+    public static GameObject CreateShotgun()
+    {
+        return null;
+    }
+
+    public static GameObject CreateGrenade()
+    {
+        return null;
+    }
+}
