324d38d42db8818e4d677c25ed2982cbfe1056fe
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-16 13:23:10 -0500

    Added arguments to unity events

--- Assets/Scripts/UnityEvents.cs
+++ Assets/Scripts/UnityEvents.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 // Type the above otherwise we have to write "UnityEngine.Events.UnityEvent onPlayerDeath;"
 
+public class PlayerDeathEvent : EventArgs
+{
+    public Vector3 deathPosition = Vector3.zero;
+}
+
 public class UnityEvents : MonoBehaviour
 {
-    UnityEvent onPlayerDeath = new UnityEvent();
+    UnityEvent<PlayerDeathEvent> onPlayerDeath = new UnityEvent<PlayerDeathEvent>();
     int deathCount = 0;
     Vector3 spawn = Vector3.right * -5.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         // Subscribe to the "player death" topic
@@ -18,7 +23,6 @@ public class UnityEvents : MonoBehaviour
         onPlayerDeath.AddListener(PlayerRespawn);
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.right * Time.deltaTime;
@@ -26,18 +30,25 @@ public class UnityEvents : MonoBehaviour
         // Dispatch player death event when space is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            onPlayerDeath.Invoke();
+            PlayerDeathEvent evt = new PlayerDeathEvent();
+            evt.deathPosition = transform.position;
+            onPlayerDeath.Invoke(evt);
         }
     }
 
-    void PlayerDeathCounter()
+    void PlayerDeathCounter(PlayerDeathEvent evt)
     {
         deathCount++;
         Debug.Log("You've died " + deathCount + " times.");
+        if (deathCount == 5)
+        {
+            Debug.Log("Achievemnt unlock: die 5 times xD xD xD xD xD");
+        }
     }
 
-    void PlayerRespawn()
+    void PlayerRespawn(PlayerDeathEvent evt)
     {
+        Debug.Log("Player died at location " + evt.deathPosition);
         Debug.Log("Respawning player at location " + spawn);
         transform.position = spawn;
     }
