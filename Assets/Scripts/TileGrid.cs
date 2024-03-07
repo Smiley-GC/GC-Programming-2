3088efbf1b0b5fc3b7c981ffabcc03d69354efae
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-07 13:58:44 -0500

    Added homework 4

--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -43,13 +43,47 @@ public class TileGrid : MonoBehaviour
             y -= 1.0f;
         }
 
+        // TODO -- color using Clairvoyance instead
+        //for (int row = 0; row < rows; row++)
+        //{
+        //    for (int col = 0; col < cols; col++)
+        //    {
+        //        Color color = types[row, col] == 1 ? Color.red : Color.white;
+        //        tiles[row][col].GetComponent<SpriteRenderer>().color = color;
+        //    }
+        //}
+    }
+
+    void Update()
+    {
+        int rows = types.GetLength(0);
+        int cols = types.GetLength(1);
+
+        // Revert colours to white every frame
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
             {
-                Color color = types[row, col] == 1 ? Color.red : Color.white;
-                tiles[row][col].GetComponent<SpriteRenderer>().color = color;
+                tiles[row][col].GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
+
+
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse = new Vector2(mouse.x, types.GetLength(0) - mouse.y);
+        // Consider using the built-in Vector2Int type when accessing a 2d array
+        //Vector2Int cell = new Vector2Int((int)mouse.x, (int)mouse.y);
+        int mouseRow = (int)mouse.y;
+        int mouseCol = (int)mouse.x;
+        tiles[mouseRow][mouseCol].GetComponent<SpriteRenderer>().color = Color.magenta;
+
+        // Colour adjacent tiles red if impassible or green if passible.
+        // Ensure you don't get any runtime errors (ie index out of range exception)
+        Clairvoyance(mouseRow, mouseCol);
+    }
+
+    void Clairvoyance(int row, int col)
+    {
+        
     }
 }
