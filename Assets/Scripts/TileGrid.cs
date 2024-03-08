0f1f447f5efc475faa0190199bc63ecba426f1f3
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-08 13:18:18 -0500

    Added homework 4 starter

--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -7,32 +7,79 @@ public class TileGrid : MonoBehaviour
     public GameObject tilePrefab;
     List<List<GameObject>> tiles = new List<List<GameObject>>();
 
+    int[,] tileTypes =
+    {
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    };
+
     void Start()
     {
+        int rows = tileTypes.GetLength(0);
+        int cols = tileTypes.GetLength(1);
         float xStart = 0.5f;
-        float yStart = 0.5f;
+        float yStart = 0.5f + (rows - 1);
         float x = xStart;
         float y = yStart;
 
-        for (int row = 0; row < 10;  row++)
+        for (int row = 0; row < rows;  row++)
         {
             List<GameObject> columns = new List<GameObject>();
-            for (int col = 0; col < 20; col++)
+            for (int col = 0; col < cols; col++)
             {
                 GameObject tile = Instantiate(tilePrefab);
                 tile.transform.position = new Vector3(x, y, 0.0f);
                 columns.Add(tile);
                 x += 1.0f;
+
+                // Color based on tile type!
+                Color color = tileTypes[row, col] == 1 ? Color.red : Color.white;
+                tile.GetComponent<SpriteRenderer>().color = color;
             }
             tiles.Add(columns);
             x = xStart;
-            y += 1.0f;
+            y -= 1.0f;
         }
     }
 
     void Update()
     {
-        tiles[0][0].GetComponent<SpriteRenderer>().color = Color.red;
-        tiles[9][19].GetComponent<SpriteRenderer>().color = Color.green;
+        // Revert colour of every tile to white
+        int rows = tileTypes.GetLength(0);
+        int cols = tileTypes.GetLength(1);
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                tiles[row][col].GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+
+        // Mouse --> screen space --> world space --> grid space
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        int mouseRow = (tileTypes.GetLength(0) - 1) - (int)mouse.y;
+        int mouseCol = (int)mouse.x;
+        //Debug.Log(mouse);
+
+        // Render adjacent tiles red if lava, green if stone
+        Clairvoyance(mouseRow, mouseCol);
+    }
+
+    // Tip: start by making a blue +, then colour tiles red/green based on condition, finally fix index errors
+    void Clairvoyance(int row, int col)
+    {
+        // Cursor column (don't change this)
+        tiles[row][col].GetComponent<SpriteRenderer>().color = Color.magenta;
+
+        // Left column
+        tiles[row][col - 1].GetComponent<SpriteRenderer>().color = Color.blue;
     }
 }
