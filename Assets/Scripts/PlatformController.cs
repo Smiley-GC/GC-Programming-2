0b72999ca84fbe9cfb55b8190fb548c86c8fa733
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-04 10:45:40 -0400

    Added wall materials

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -6,6 +6,10 @@ public class PlatformController : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    const int jumpCount = 2;
+    int jumps = jumpCount;
+    bool grounded = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -13,8 +17,9 @@ public class PlatformController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumps > 0)
         {
+            jumps--;
             rb.AddForce(Vector3.up * 10.0f, ForceMode2D.Impulse);
         }
         if (Input.GetKey(KeyCode.A))
@@ -24,6 +29,25 @@ public class PlatformController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(Vector2.right * 1.0f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.name);
+        //Debug.Log(name);
+        if (collision.CompareTag("Ground"))
+        {
+            jumps = jumpCount;
+            grounded = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            grounded = false;
         }
     }
 }
