7838892eceae8018819b5961136360a0d26bad8d
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-04 13:41:28 -0400

    Added walls and roof

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
     }
 }
