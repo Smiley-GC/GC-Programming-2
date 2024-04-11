c00eb175fadaeea3f87a6dc456b9c2913079e8bd
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-11 13:42:23 -0400

    Improved comments

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
-    // An enum is a more "correct" solution, but C# doesn't like casting enums to integers (for array access)...
-    //const int IDLE = 0;
-    //const int WALK = 1;
-    //const int RUN = 2;
-    //const int JUMP = 3;
     enum AnimationType
     {
         IDLE,
@@ -23,8 +18,9 @@ public class PlayerAnimationController : MonoBehaviour
     List<AnimationClip> clips = new List<AnimationClip>();
     List<float> speeds = new List<float>();
     float turnSpeed = 250.0f;
-    float moveSpeed = 5.0f;
 
+    // Optional homework 1: add a mechanism to prevent animation-switching while the player is jumping.
+    // Optional homework 2: implement a way to change animation speed that uses our AnimationType enum instead of strings.
     void Start()
     {
         animation = GetComponent<Animation>();
@@ -36,7 +32,7 @@ public class PlayerAnimationController : MonoBehaviour
         speeds.Add(0.0f);
         speeds.Add(5.0f);
         speeds.Add(10.0f);
-        speeds.Add(69.0f);  // jump height?
+        speeds.Add(69.0f);  // jump not in use currently
         animation.clip = clips[(int)AnimationType.IDLE];
     }
 
@@ -62,30 +58,18 @@ public class PlayerAnimationController : MonoBehaviour
             translation -= 1.0f;
         }
 
-        // Optional homework 1: add a mechanism to prevent animation-switching while the player is jumping (otherwise animations glitch)
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    type = AnimationType.JUMP;
-        //}
-        
-        // Easiest way would be to add some sort of jumping check here
+        // Determine state based on locomotion values & input
         type = translation == 0.0f ? AnimationType.IDLE : AnimationType.WALK;
         if (type == AnimationType.WALK && Input.GetKey(KeyCode.LeftShift))
             type = AnimationType.RUN;
 
+        // Animate the player via state
         animation.clip = clips[(int)type];
+        animation.Play();
+
+        // Locomote the player via state
         translation *= speeds[(int)type];
-
-        // Optional homework 2: implement a way to change animation speed that uses our AnimationType enum instead of strings.
-
-        // Manual locomotion (involves understanding vectors & quaternions
         transform.rotation *= Quaternion.Euler(0.0f, rotation * dt, 0.0f);
         transform.position += transform.forward * translation * dt;
-
-        // Automatic locomotion (doesn't involve anything xD)
-        //transform.Rotate(0.0f, rotation * dt, 0.0f);
-        //transform.Translate(Vector3.forward * translation * dt);
-        //transform.Translate(transform.forward * translation * dt, Space.World);
-        animation.Play();
     }
 }
